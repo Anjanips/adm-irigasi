7772bd641f8580bb29e36a8f142fcaa3adc1ad09
Anjanips <143574069+Anjanips@users.noreply.github.com>
2025-03-05 11:38:30 +0700

    Add files via upload

--- pages/aset/acimulu.vue
+++ pages/aset/acimulu.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="card mb-3">
-            <img src="assets/img/acimulu.jpg" class="card-img-top" alt="..." style="height: 500px; width: 500px;">
+            <img src="assets/img/acimulu.jpg" class="card-img-top" alt="..." style="height: 800px; width: 800px;">
         </div>
 </template>
